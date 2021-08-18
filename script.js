06f1d8811ae43bb1d70d90966942f9807285e588
j.niemiec@sente.pl <j.niemiec@sente.pl>
2021-08-18 15:24:25 +0200

    add all sections

--- script.js
+++ script.js
@@ -6,6 +6,100 @@ $(function () {
     })
 })
 
+const handleConnector = () => {
+    var options = document.querySelectorAll('.activity-option')
+    options.forEach((element) => {
+        var col = element.parentNode.parentNode.nextElementSibling
+
+        if (
+            element.parentNode.children[0].checked &&
+            col?.classList.contains('activity-col') &&
+            col?.children[0]?.children[0]?.checked
+        ) {
+            element.nextElementSibling.classList.add('visible')
+        } else {
+            element.nextElementSibling.classList.remove('visible')
+        }
+    })
+}
+
+$(function () {
+    handleConnector()
+})
+
+var toggleChecked = function (col) {
+    if (col?.parentNode?.children[0]) {
+        col.parentNode.children[0].checked = !col.parentNode.children[0].checked
+        handleConnector()
+    }
+}
+
+$(function () {
+    var options = document.querySelectorAll('.activity-option')
+    options.forEach((element) => {
+        var col = element
+
+        const myFunction = (tooltipSpan) => {
+            // console.log(e.target.tagName.toLowerCase() == 'span')
+            if (tooltipSpan.tagName.toLowerCase() == 'span') {
+                console.log('show')
+                $('[data-toggle="tooltip"]').tooltip('hide')
+                $('[data-toggle="tooltip"]').filter(tooltipSpan).tooltip('show')
+            }
+        }
+
+        col.addEventListener('mouseover', function (e) {
+            if (e.buttons == 1 || e.buttons == 3) {
+                toggleChecked(col)
+                myFunction(e.target.parentNode.parentNode.parentNode.parentNode)
+            }
+        })
+        col.addEventListener('mousedown', function (e) {
+            toggleChecked(col)
+            myFunction(e.target.parentNode.parentNode.parentNode.parentNode)
+        })
+
+        col.parentNode.children[0].addEventListener('click', function (e) {
+            toggleChecked(col)
+            // myFunction(e.target.parentNode.parentNode.parentNode.parentNode)
+        })
+    })
+})
+
+$(function () {
+    $('#exclusions-input').on('focusin', (event) => {
+        event.target.parentNode.children[0].classList.remove('hidden')
+    })
+})
+
+// $(function () {
+//     $('#search-result').on('focusin', (event) => {
+//         console.log(e.target.height)
+//     })
+// })
+// $(function () {
+//     $('#search-result').on('focusin', (event) => {
+//         event.target.parentNode.children[0].classList.remove('hidden')
+//     })
+// })
+document.addEventListener('click', function (e) {
+    if (e.target != $('#exclusions-input')[0]) {
+        let element = e.target
+        let shouldClose = true
+        while (element != null) {
+            if (element == $('.search-results-container')[0]) {
+                shouldClose = false
+            }
+            element = element.parentNode
+        }
+        if (shouldClose === true) {
+            $('#exclusions-input')[0].parentNode.children[0].classList.add(
+                'hidden'
+            )
+        }
+    }
+})
+
 function makeSidebarItemSelected(el, itemName) {
     let list = el.parentNode.parentNode.children
     for (let item of list) {
@@ -45,8 +139,103 @@ function makeSidebarItemSelected(el, itemName) {
     return el.classList.add('selected')
 }
 
+const binMouseEnter = (el) => {
+    el.children[0].src = 'images/red-bin.svg'
+}
+const binMouseOut = (el) => {
+    el.children[0].src = 'images/bin.svg'
+}
+
 function toggleSelect(el) {
     return el.classList.toggle('selected')
+}
+function toggleActive(el) {
+    let menuItems = el.parentNode.parentNode.children
+    for (i = 0; i < menuItems.length; i++) {
+        menuItems[i].children[0].classList.remove('active')
+    }
+    if (el.innerHTML.indexOf('wykluczeń') != -1) {
+        //wykluczenia
+        $('#exclusions-list')[0].classList.remove('hidden')
+        $('#exceptions-list')[0].classList.add('hidden')
+    } else {
+        //wyjątki
+        $('#exclusions-list')[0].classList.add('hidden')
+        $('#exceptions-list')[0].classList.remove('hidden')
+    }
+    return el.classList.add('active')
+}
+
+function remove(el) {
+    return el.parentNode.removeChild(el)
+}
+
+function focusDateInput() {
+    document.getElementById('datepicker').focus()
+}
+
+function addToExclusions(el) {
+    var text = document.createTextNode(
+        el.parentNode.children[1].children[0].textContent
+    )
+    var div = document.createElement('div')
+    var p = document.createElement('p')
+    var img = document.createElement('img')
+
+    div.classList.add(
+        'round-label',
+        'd-flex',
+        'justify-content-center',
+        'align-items-center'
+    )
+    div.onclick = (el) =>
+        remove(
+            el.target.classList.contains('round-label')
+                ? el.target
+                : el.target.parentNode
+        )
+    img.classList.add('navbar-toggle-icon')
+    img.src = 'images/x.svg'
+    img.alt = 'x-icon'
+    p.appendChild(text)
+    div.appendChild(p)
+    div.appendChild(img)
+
+    $('#exclusions-list').append(div)
+
+    $('#exclusions-input')[0].parentNode.children[0].classList.add('hidden')
+}
+
+function addToExceptions(el) {
+    var text = document.createTextNode(
+        el.parentNode.children[1].children[0].textContent
+    )
+    var div = document.createElement('div')
+    var p = document.createElement('p')
+    var img = document.createElement('img')
+
+    div.classList.add(
+        'round-label',
+        'd-flex',
+        'justify-content-center',
+        'align-items-center'
+    )
+    div.onclick = (el) =>
+        remove(
+            el.target.classList.contains('round-label')
+                ? el.target
+                : el.target.parentNode
+        )
+    img.classList.add('navbar-toggle-icon')
+    img.src = 'images/x.svg'
+    img.alt = 'x-icon'
+    p.appendChild(text)
+    div.appendChild(p)
+    div.appendChild(img)
+
+    $('#exceptions-list').append(div)
+
+    $('#exclusions-input')[0].parentNode.children[0].classList.add('hidden')
 }
 
 document.addEventListener('DOMContentLoaded', function () {
@@ -74,3 +263,83 @@ document.addEventListener('DOMContentLoaded', function () {
         })
     }
 })
+$('.datepicker').datepicker()
+$(document).ready(function () {
+    $.fn.datepicker.dates['en'] = {
+        days: [
+            'Niedziela',
+            'Poniedziałek',
+            'Wtorek',
+            'Środa',
+            'Czwartek',
+            'Piątek',
+            'Sobota',
+        ],
+        daysShort: ['Ndz', 'Pon', 'Wt', 'Śr', 'Czw', 'Pt', 'Sob'],
+        daysMin: ['N', 'Pn', 'Wt', 'Śr', 'Cz', 'Pt', 'S'],
+        months: [
+            'Styczeń',
+            'Luty',
+            'Marzec',
+            'Kwiecień',
+            'Maj',
+            'Czerwiec',
+            'Lipiec',
+            'Sierpień',
+            'Wrzesień',
+            'Październik',
+            'Listopad',
+            'Grudzień',
+        ],
+        monthsShort: [
+            'Sty',
+            'Lut',
+            'Mar',
+            'Kwi',
+            'Maj',
+            'Cze',
+            'Lip',
+            'Sie',
+            'Wrz',
+            'Paź',
+            'Lis',
+            'Gru',
+        ],
+        today: 'Dziś',
+        clear: 'Wyczyść',
+        format: 'mm/dd/yyyy',
+        titleFormat: 'MM yyyy' /* Leverages same syntax as 'format' */,
+        weekStart: 1,
+        startDate: '12-12-2021',
+    }
+
+    $('#datepicker').datepicker({
+        format: 'dd-mm-yyyy',
+    })
+    $.fn.datepicker.defaults.startDate = '10/10/2021'
+
+    $('#datepicker').datepicker('setDate', new Date())
+})
+
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+})
+
+$(function () {
+    $('[data-toggle="tooltip"]').on('show.bs.tooltip', (e) =>
+        refreshTooltip(e.target)
+    )
+})
+
+function refreshTooltip(e) {
+    let row = e.children[0]
+    let timeLength = row.children[1].children[0].value
+    let quantity = 0
+    for (i = 0; i < row.children.length; i++) {
+        if (row.children[i]?.children[0]?.children[0]?.checked) {
+            quantity++
+        }
+    }
+    let sum = quantity * timeLength
+    e.setAttribute('data-original-title', 'suma: ' + sum + ' godz.')
+}
